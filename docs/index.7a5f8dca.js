7d7e7854cb1b057e2178941220804a59c3afa21a
LucasvdVegt <lpsvandervegt@gmail.com>
2022-06-20 22:22:00 +0200

    add collison

--- docs/index.7a5f8dca.js
+++ docs/index.7a5f8dca.js
@@ -583,7 +583,15 @@ class Game {
     update(delta) {
         this.player.update();
         this.ui.update();
-        for (let robot1 of this.robot1s)robot1.update(delta);
+        for (let robot1 of this.robot1s){
+            robot1.update(delta);
+            if (this.collision(robot1, this.player)) robot1.randomLocation();
+        }
+    }
+    collision(sprite1, sprite2) {
+        const bounds1 = sprite1.getBounds();
+        const bounds2 = sprite2.getBounds();
+        return bounds1.x < bounds2.x + bounds2.width && bounds1.x + bounds1.width > bounds2.x && bounds1.y < bounds2.y + bounds2.height && bounds1.y + bounds1.height > bounds2.y;
     }
 }
 new Game();
